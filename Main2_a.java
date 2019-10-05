5187cf674fa6e4d3db30d25727085020e793a5f0
Marlena Piekarz <foreverrebel95@gmail.com>
2019-10-05 12:44:10 -0400

    komenatrze do zadania 2

--- Main2_a.java
+++ Main2_a.java
@@ -4,6 +4,8 @@ public class Main2_a {
 	public static void main(String[] args) {
 		System.out.println("Pierwszy program JAVA");
 
+// błądem był brak ; na końcu linijki
+
 	}
 
 }
--- Main2_b.java
+++ Main2_b.java
@@ -4,6 +4,8 @@ public class Main2_b {
 	public static void main(String[] args) {
 		System.out.println("Pierwszy program JAVA");
 
+// błąd składniowy w metodzie "pintln"
+
 	}
 
 }
