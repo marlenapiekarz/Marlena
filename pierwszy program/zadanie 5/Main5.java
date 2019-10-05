af2c5e5cc721a2d3c1aeb9c6a0ef03ba96995bcc
marlenapiekarz <marlena.renata.piekarz@gmail.com>
2019-10-05 13:13:41 -0400

    zadanie 5

--- pierwszy program/zadanie 5/Main5.java
+++ pierwszy program/zadanie 5/Main5.java
@@ -1,6 +1,6 @@
 
-public class CodersLab {
-
+public class Main5 {
+// klasa powinna nazywać sie tak samo jak plik
 	public static void main(String[] args) {
 		System.out.print("Dzisiaj zaczynam kurs");
 		System.out.print("języka JAVA");
