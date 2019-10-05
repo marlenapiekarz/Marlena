b468190a6dc98f8e3bb43752cae0bbd4ecad34cb
Marlena Piekarz <foreverrebel95@gmail.com>
2019-10-05 12:35:49 -0400

    zadanie 1 2 3

---
+++ Main3.java
@@ -0,0 +1,13 @@
+// komentarz jednolinijkowy
+/*
+komenatrz wielolinijkowy
+*/
+/** komentarz dokumentacyjny */
+public class Main3 {
+
+	public static void main(String[] args) {
+		System.out.println("Pierwszy program JAVA");
+
+	}
+
+}
