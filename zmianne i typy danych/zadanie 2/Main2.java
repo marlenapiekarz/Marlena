969bbce8b80d4b68effbc814782890838c8f820b
marlenapiekarz <marlena.renata.piekarz@gmail.com>
2019-10-06 13:57:14 -0400

    zadanie 2

---
+++ zmianne i typy danych/zadanie 2/Main2.java
@@ -0,0 +1,11 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		int nr1 = 10;
+		int nr2 = 20;
+		int result = 0;
+		result = nr1 + nr2;
+		System.out.println(result);
+	} 
+}
