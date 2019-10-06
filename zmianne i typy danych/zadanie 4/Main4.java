62da39672c43d09086a1dcbef65ae6c9c86e2195
marlenapiekarz <marlena.renata.piekarz@gmail.com>
2019-10-06 14:12:33 -0400

    zadanie 4

---
+++ zmianne i typy danych/zadanie 4/Main4.java
@@ -0,0 +1,16 @@
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		int number1;
+		int number2;
+		char char1 = '1';
+		char char2 = '7';
+		boolean boolean1;
+		boolean1 = true;
+		boolean boolean2;
+		boolean2 = false;
+
+	}
+
+}
