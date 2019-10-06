1e52547c38f8a9a20268dc7d551575d8e08e7d8a
marlenapiekarz <marlena.renata.piekarz@gmail.com>
2019-10-06 14:48:48 -0400

    zadanie 3

---
+++ operatory/zadanie 3/Main3.java
@@ -0,0 +1,13 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		String str1 = "Kurs";
+		String str2 = "Java";
+		String joinedStrings;
+		joinedStrings = str1 + str2;
+		System.out.println(joinedStrings);
+
+	}
+
+}
