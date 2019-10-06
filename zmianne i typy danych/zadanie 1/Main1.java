2cef30d9d0e7ea4613b0f869781e65d1f481953a
marlenapiekarz <marlena.renata.piekarz@gmail.com>
2019-10-06 11:58:14 -0400

    zadanie 1

---
+++ zmianne i typy danych/zadanie 1/Main1.java
@@ -0,0 +1,13 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int value = 34;
+		char value = 'b';
+		long value = 12345678910L;
+		double value = 4.94;
+		boolean value = true;
+		
+	}
+
+}
