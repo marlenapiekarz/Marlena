9158890b2d15e99b77286613b6bc8a0f425484e3
marlenapiekarz <marlena.renata.piekarz@gmail.com>
2019-10-06 14:04:22 -0400

    zadanie 3

---
+++ zmianne i typy danych/zadanie 3/Main3.java
@@ -0,0 +1,13 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		int nr1 = 10;
+		double nr2 = 11.37;
+		double result = 0;
+		result = nr1 + nr2;
+		System.out.println(result);
+		
+	}
+
+}
